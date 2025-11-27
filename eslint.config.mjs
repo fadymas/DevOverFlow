5362c08f7c3c7299ac91f1100395daea0ff49920
Fady Mahrous <fadymahros373@gmail.com>
2025-11-28 01:24:05 +0200

    configure the development environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import promisePlugin from 'eslint-plugin-promise'
+import importPlugin from 'eslint-plugin-import'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default defineConfig([
+  // --- Next.js rules ---
+  ...nextCoreWebVitals,
+  ...nextTypescript,
 
-export default eslintConfig;
+  // --- Plugins (flat style) ---
+  {
+    plugins: {
+      promise: promisePlugin,
+      import: importPlugin
+    },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+      ...promisePlugin.configs.recommended.rules
+    }
+  },
+
+  // --- Prettier (must be last to disable conflicting rules) ---
+  prettier,
+
+  // --- Ignores ---
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])
+])
